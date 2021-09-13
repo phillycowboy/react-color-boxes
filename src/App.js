3c645772d62dd0ed638dc9c98ab70a48338070a6
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-09-13 17:14:27 -0400

    added file for helper method

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
+import Box from './Box'
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Box />
     </div>
   );
 }
--- src/Box.js
+++ src/Box.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
-
+import {choice} from './helpers'
 
 class Box extends Component{
+    static defaultProps = {
+        allColors: ['purple', 'magenta', 'violet', 'pink']
+    }
     constructor(props){
         super(props)
         this.state = {
-            color: 'purple'
+            color: choice(this.props.allColors)
         }
+    }
+    pickColor = () => {
+        let newColor;
+        do {
+          newColor = choice(this.props.allColors)  
+        } while (newColor === this.state.color)
+        // never the same color twice 
+        this.setState({ color: newColor})
     }
 
     handleClick = () => {
-        
+        this.pickColor()
     }
     render(){
         return(
