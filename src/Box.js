4ac812f41fcbb15dac2dedc6367d0ff49292b0f0
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-09-13 16:59:28 -0400

    made box component

---
+++ src/Box.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+
+class Box extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            color: 'purple'
+        }
+    }
+
+    handleClick = () => {
+        
+    }
+    render(){
+        return(
+            <div className="Box" style={{backgroundColor: this.state.color}} onClick={this.handleClick}>
+    
+            </div>
+        )
+    }
+}
+export default Box
